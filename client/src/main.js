66ee582bca3b27f3d4f8b97aa1035e64a898506d
Sean Knowles <sean.code@outlook.com>
2018-10-07 22:52:18 +0200

    again fixed breaking changes and bugs regarding ApolloClient and webpack by adding vue.config.js file

--- client/src/main.js
+++ client/src/main.js
@@ -1,16 +1,28 @@
-import '@babel/polyfill'
-import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import "@babel/polyfill";
+import Vue from "vue";
+import "./plugins/vuetify";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+import ApolloClient from "apollo-boost";
+import VueApollo from "vue-apollo";
 
-Vue.config.productionTip = false
+Vue.use(VueApollo);
+
+// Setup ApolloClient and connect to backend
+const defaultClient = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
+const apolloProvider = new VueApollo({ defaultClient });
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  apolloProvider,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
