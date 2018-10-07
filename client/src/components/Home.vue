506cad3ec03943a5a37c2af0bce959d96a9336a9
Sean Knowles <sean.code@outlook.com>
2018-10-07 23:32:01 +0200

    started calling backend from client home component.

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,14 +1,44 @@
 <template>
   <div>
-    <h1>Home</h1>
+    <v-container>
+      <h1>Home</h1>
+      <ul v-for="post in getPosts" :key="post.id">
+        <li>
+          {{ post.title }}
+        </li>
+        <li>
+          {{ post.imageUrl }}
+        </li>
+        <li>
+          {{ post.description }}
+        </li>
+      </ul>
+    </v-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import { gql } from "apollo-boost";
 
 export default {
   name: "Home",
-  components: {}
+  updated() {
+    console.log(this.getPosts);
+  },
+  apollo: {
+    getPosts: {
+      query: gql`
+        query {
+          getPosts {
+            title
+            imageUrl
+            description
+            likes
+          }
+        }
+      `
+    }
+  }
 };
 </script>
