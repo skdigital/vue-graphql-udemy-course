bdf3ff3bc6c54a6d8e2bedd2961e122d409747c3
Sean Knowles <sean.code@outlook.com>
2018-10-07 16:59:26 +0200

    error fixes, npm and package.json file edited, app is serving error free

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
-import colors from "vuetify/es5/util/colors";
 
 Vue.use(Vuetify, {
   iconfont: "md",
