3f6138220c165539b25e54429ad7cfc1a60fa8b5
Sean Knowles <sean.code@outlook.com>
2018-10-10 13:12:42 +0200

    implemented signup, signin user with password checks and encryption and added token return type on resolver functions

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const md5 = require("md5");
+const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -29,6 +31,30 @@ const UserSchema = new mongoose.Schema({
     required: true,
     ref: "Post"
   }
+});
+
+// Pre save hooks before user is saved to the database mLab
+// Create and add avatar to user then continues to next function using next()
+UserSchema.pre("save", function(next) {
+  this.avatar = `http://gravater.com/avatar/${md5(this.username)}?d=identicon`;
+  next();
+});
+
+// Hash password so it can't be seen w/ acces to database
+UserSchema.pre("save", function(next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err);
+
+      this.password = hash;
+      next();
+    });
+  });
 });
 
 module.exports = mongoose.model("User", UserSchema);
--- resolvers.js
+++ resolvers.js
@@ -1,3 +1,11 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const createToken = (user, secret, expiresIn) => {
+  const { username, email } = user;
+  return jwt.sign({ username, email }, secret, { expiresIn });
+};
+
 module.exports = {
   Query: {
     getPosts: async (_, args, { Post }) => {
@@ -26,6 +34,20 @@ module.exports = {
       }).save();
       return newPost;
     },
+    signinUser: async (_, { username, password }, { User }) => {
+      // find user with supplied username
+      // then checks if user exists if not thow error
+      const user = await User.findOne({ username });
+      if (!user) {
+        throw new Error("User not found");
+      }
+      // check user supplied password against encrypted stored password
+      const isValidPassword = await bcrypt.compare(password, user.password);
+      if (!isValidPassword) {
+        throw new Error("Invalid Password");
+      }
+      return { token: createToken(user, process.env.SECRET, "1hr") };
+    },
     signupUser: async (_, { username, email, password }, { User }) => {
       const user = await User.findOne({ username });
       if (user) {
@@ -36,7 +58,7 @@ module.exports = {
         email,
         password
       }).save();
-      return newUser;
+      return { token: createToken(newUser, process.env.SECRET, "1hr") };
     }
   }
 };
