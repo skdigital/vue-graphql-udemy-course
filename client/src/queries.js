79786189b672fb732c8d16a183c1227f278ef0e3
Sean Knowles <sean.code@outlook.com>
2018-10-10 13:47:34 +0200

    added signinUser and signupUser client queries

--- client/src/queries.js
+++ client/src/queries.js
@@ -21,3 +21,19 @@ export const GET_POSTS = gql`
 /* Post Mutations */
 
 /* User Mutations */
+
+export const SIGNIN_USER = gql`
+  mutation($username: String!, $password: String!) {
+    signinUser(username: $username, password: $password) {
+      token
+    }
+  }
+`;
+
+export const SIGNUP_USER = gql`
+  mutation($username: String!, $email: String!, $password: String!) {
+    signupUser(username: $username, email: $email, password: $password) {
+      token
+    }
+  }
+`;
